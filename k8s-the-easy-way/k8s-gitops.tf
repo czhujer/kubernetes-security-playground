1f2a971a5aab45c7ddb4b1d7ac18c16397a3cb42
Patrik Majer <patrik.majer.pisek@gmail.com>
2022-04-29 22:22:48 +0200

    tf: fix structure of files

--- k8s-the-easy-way/k8s-gitops.tf
+++ k8s-the-easy-way/k8s-gitops.tf
@@ -1,21 +1,5 @@
-
-resource "helm_release" "cert-manager" {
-  name             = "cert-manager"
-  repository       = "https://charts.jetstack.io"
-  chart            = "cert-manager"
-  version          = "1.8.0"
-  namespace        = "cert-manager"
-  create_namespace = "true"
-
-  values = [
-    file("../kind/cert-manager.yaml")
-  ]
-
-  depends_on = [digitalocean_droplet.control_plane,
-    digitalocean_droplet.worker,
-  ]
-}
-
+# gitops stuff
+#
 resource "helm_release" "argocd" {
   name             = "argocd-single"
   repository       = "https://argoproj.github.io/argo-helm"
---
+++ k8s-the-easy-way/k8s-system.tf
@@ -0,0 +1,20 @@
+## system stuff
+#
+resource "helm_release" "cert-manager" {
+  name             = "cert-manager"
+  repository       = "https://charts.jetstack.io"
+  chart            = "cert-manager"
+  version          = "1.8.0"
+  namespace        = "cert-manager"
+  create_namespace = "true"
+
+  values = [
+    file("../kind/cert-manager.yaml")
+  ]
+
+  depends_on = [digitalocean_droplet.control_plane,
+    digitalocean_droplet.worker,
+  ]
+}
+
+# ingress
