ca94b50a64e0eca50a7b41745734242847694bf5
Patrik Majer <patrik.majer.pisek@gmail.com>
2022-06-14 20:43:44 +0200

    bump loki and cert-manager versions

--- k8s-the-easy-way/k8s-system.tf
+++ k8s-the-easy-way/k8s-system.tf
@@ -229,7 +229,7 @@ resource "helm_release" "cert-manager" {
   name             = "cert-manager"
   repository       = "https://charts.jetstack.io"
   chart            = "cert-manager"
-  version          = "1.8.0"
+  version          = " v1.8.1 "
   namespace        = "cert-manager"
   create_namespace = "true"
 
